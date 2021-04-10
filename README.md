c74cd816f60f7837fe3e3c20ef053f014ddd9840
lingluo-maple <1556566021@qq.com>
2021-04-10 14:16:10 +0800

    update

--- README.md
+++ README.md
@@ -3,14 +3,15 @@
 
 这是一个用Python编写的QQ机器人
 
-### 目前功能
- - 入群欢迎
- - 手动禁言~~没什么用的功能~~
- - 获取机器人加入的群聊
+## 目前功能
 
-#### 你需要在**config.yml**文件中修改配置
+- 入群欢迎
+- 手动禁言~~没什么用的功能~~
+- 获取机器人加入的群聊
 
-### Config
+### 你需要在**config.yml**文件中修改配置
+
+## Config
 
 参数  | 说明
 -------  | -----
@@ -20,7 +21,8 @@ DebugGroup | 功能测试用群号
 Group | 机器人运行的群号
 Image | 需要发送的图片的路径
 
-#### 感谢以下项目
+## 感谢以下项目
+
 - [Mirai---使用的QQ机器人框架](https://github.com/mamoe/mirai)
 - [mirai-api-http](https://github.com/project-mirai/mirai-api-http)
 - [Graia Application](https://github.com/GraiaProject/Application)
--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ from graia.application.friend import Friend
 from graia.application.group import Group, Member
 
 from GetConfig import GetConfig, GetMahConfig
+from GroupMute import group_mute
 
 try:
     mah_config = GetMahConfig()
@@ -48,60 +49,6 @@ async def group_permission(app: GraiaMiraiApplication, group: Group, member: Mem
         Plain("您对机器人的权限为:"), Plain(perm)
     ]))
     
-async def group_mute(app: GraiaMiraiApplication,group,member,message):
-    user = str(member.permission)
-    if user == 'MemberPerm.Member':
-    #发言者权限为成员
-        await app.sendGroupMessage(group,MessageChain.create([
-            Plain("你的权限不足")
-        ]))
-        return False
-    bot = str(group.accountPerm)
-    if bot == 'MemberPerm.Member':
-    #机器人权限为成员
-        await app.sendGroupMessage(group,MessageChain.create([
-            Plain("机器人权限不足")
-        ]))
-        return False
-    try:
-        beMute = message.get(At)[0].target
-    except IndexError:
-        await app.sendGroupMessage(group,MessageChain.create([
-            Plain("参数错误,请输入要禁言的人")
-        ]))
-        return False
-    try:
-        muteTime = int(message.asDisplay()[5:])
-    except ValueError:
-        await app.sendGroupMessage(group,MessageChain.create([
-            Plain("参数错误,请设置禁言时间(单位:秒)")
-        ]))
-        return False
-    if beMute == config["host"]:
-        await app.sendGroupMessage(group,MessageChain.create([
-            Plain("禁言失败，请选择其他人")
-        ]))
-    elif beMute == config["Robot"]:
-        await app.sendGroupMessage(group,MessageChain.create([
-            Plain("禁言失败，机器人不可以禁言自己")
-        ]))
-    elif muteTime <= 0 or muteTime >= 2592000:
-        await app.sendGroupMessage(group,MessageChain.create([
-            Plain("参数错误，禁言时间应大于0小于259200")
-        ]))
-    elif user == 'MemberPerm.Administrator' or user == 'MemberPerm.Owner' and beMute != config["Host"]:
-    #发言者权限为管理员或群主
-        try:
-            await app.mute(group,beMute,muteTime)
-        except PermissionError:
-            await app.sendGroupMessage(group,MessageChain.create([
-                Plain("权限错误")
-            ]))
-            return False
-        await app.sendGroupMessage(group,MessageChain.create([
-            Plain("已禁言"), Plain(beMute), Plain("\n时间:"), Plain(muteTime), Plain("(秒)")
-        ]))
-
 
 async def MCbe_Service(app: GraiaMiraiApplication, group: Group, member: Member, message: MessageChain):
     await app.sendGroupMessage(group,MessageChain.create([
